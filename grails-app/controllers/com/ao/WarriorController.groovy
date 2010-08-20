7ab4b2ac890cafaabc449fa80e6aa5a482eb36cf
BySouleater <bysouleater@gmail.com>
2010-08-19 21:08:58 -0300

    Pantalla principal con imagenes de guerreros

--- grails-app/controllers/com/ao/WarriorController.groovy
+++ grails-app/controllers/com/ao/WarriorController.groovy
@@ -8,44 +8,44 @@ import org.w3c.dom.Document
 class WarriorController {
 	
 	static final String FB_APP_URL = "http://apps.facebook.com/aristeia_online/"
-		static final String FB_APP_ADD_URL = "http://www.facebook.com/add.php?api_key="
-		static final String FB_API_KEY = "41f82a404dc9b0bbb4579a993c51ae4d"
-		static final String FB_SECRET_KEY = "c5b463359f752c1070dcd15db30cdcf9"
+	static final String FB_APP_ADD_URL = "http://www.facebook.com/add.php?api_key="
+	static final String FB_API_KEY = "41f82a404dc9b0bbb4579a993c51ae4d"
+	static final String FB_SECRET_KEY = "c5b463359f752c1070dcd15db30cdcf9"
 		
-		def getAuthenticatedFacebookClient(def request, def response){
-			Facebook fb = new Facebook(request, response,FB_API_KEY, FB_SECRET_KEY)
-			String next = request.getServletPath().substring(1)
-			
-			if (fb.requireLogin(next))
-				return null
-			return fb.getFacebookRestClient()
-		}
+	def getAuthenticatedFacebookClient(def request, def response){
+		Facebook fb = new Facebook(request, response,FB_API_KEY, FB_SECRET_KEY)
+		String next = request.getServletPath().substring(1)
+		
+		if (fb.requireLogin(next))
+			return null
+		return fb.getFacebookRestClient()
+	}
 
-    def index = { }
-    
-    def create = {
+	def index = { }
+
+	def create = {
 		FacebookRestClient facebook = getAuthenticatedFacebookClient(request, response)
 		if(facebook){
 			if(getFacebookInfo(request, facebook)){
 				[]
 			}	
 		}
-    }
-    
-    def save = {
+	}
+
+	def save = {
 		FacebookRestClient facebook = getAuthenticatedFacebookClient(request, response)
-		if(facebook){
-			if(getFacebookInfo(request, facebook)){
-				def warrior = new Warrior(name:params.name,owner_id:request.getAttribute("uid") as Long)
-		    	if(warrior.validate()){
-		    		warrior.save()
-		    	}
-		    	redirect(controller:"main",action:"index")
-			}	
-		}
-    }
-    
-    /*
+			if(facebook){
+				if(getFacebookInfo(request, facebook)){
+					def warrior = new Warrior(name:params.name,owner_id:request.getAttribute("uid") as Long)
+						if(warrior.validate()){
+							warrior.save()
+						}
+					redirect(controller:"main",action:"index")
+				}	
+			}
+	}
+
+	/*
 	 * This method obtains some basic Facebook profile
 	 * information from the logged in user who is
 	 * accessing our application in the current HTTP request.
@@ -53,30 +53,30 @@ class WarriorController {
 	private boolean getFacebookInfo(def request, FacebookRestClient facebook){
 		try {
 			long userID = facebook.users_getLoggedInUser()
-			Collection<Long> users = new ArrayList<Long>()
-			users.add(userID)
-			
-			
-			EnumSet<ProfileField> fields = EnumSet.of (
-					com.facebook.api.ProfileField.NAME,
-					com.facebook.api.ProfileField.PIC);
-			
+				Collection<Long> users = new ArrayList<Long>()
+				users.add(userID)
+
+
+				EnumSet<ProfileField> fields = EnumSet.of (
+						com.facebook.api.ProfileField.NAME,
+						com.facebook.api.ProfileField.PIC);
+
 			Document d = facebook.users_getInfo(users, fields);
 			String name =
-					d.getElementsByTagName("name").item(0).getTextContent();
+				d.getElementsByTagName("name").item(0).getTextContent();
 			String picture =
-					d.getElementsByTagName("pic").item(0).getTextContent();
-			
+				d.getElementsByTagName("pic").item(0).getTextContent();
+
 			request.setAttribute("uid", userID);
 			request.setAttribute("profile_name", name);
 			request.setAttribute("profile_picture_url", picture);
 		} catch (FacebookException e) {
-			
-//			HttpSession session = request.getSession();
-//			session.setAttribute("facebookSession", null);
+
+			//			HttpSession session = request.getSession();
+			//			session.setAttribute("facebookSession", null);
 			return false;
 		} catch (IOException e) {
-			
+
 			e.printStackTrace();
 			return false;
 		}
--- grails-app/views/main/index.gsp
+++ grails-app/views/main/index.gsp
@@ -3,17 +3,23 @@
        
     </head>
     <body>
-    	<g:if test="${warriorlist}">
-			<ul>
-	        	<g:each in="${warriorlist}">
-	     			<li>${it.name}</li>
-				</g:each>
-	        </ul>
-		</g:if>
-		<g:else>
+    	<h2>Guerreros</h2>	
+
+	<g:if test="${warriorlist}">
+	       	<g:each in="${warriorlist}">
+			<div style="float:left;width:250px">
+				<img style="float:left" src="/images/male_paladin.png"/>
+				<div style="float:left;width:120px">${it.name}</div>
+				<div style="float:left;width:120px">HP: ${5} / ${15}</div>
+				<div style="float:left;width:120px">MP: ${5} / ${15}</div>
+				<div style="float:left;width:120px">Paladin Lv. ${99}</div>
+			</div> 	
+		</g:each>
+	</g:if>
+	<g:else>
      		<div>No tienes ningun guerrero.</div>
-		</g:else>
-		
-		<div><g:link action="create" controller="warrior">Crear un guerrero</g:link></div>
+	</g:else>
+	<div style="clear:both"></div>		
+	<div style="float:right"><g:link action="create" controller="warrior">Crear un guerrero</g:link></div>
     </body>
 </html>
